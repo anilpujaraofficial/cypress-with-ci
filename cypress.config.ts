de0f575c8ef61d5575b0c55725b0231f14c969b6
Anil Pujara <anilpujara125@gmail.com>
2024-11-19 10:37:37 +0545

    update

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,5 +5,14 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+
+    reporter: "mochawesome",
+    reporterOptions: {
+      reportDir: "cypress/reports",
+      overwrite: false,
+      html: true,
+      json: true,
+    },
+    env: {},
   },
 });
