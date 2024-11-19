ad88355e7951550e31c05f05f071dc346ff55365
Anil Pujara <anilpujara125@gmail.com>
2024-11-19 11:47:23 +0545

    update

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-
+    video: true,
     reporter: "mochawesome",
     reporterOptions: {
       reportDir: "cypress/reports/mocha",
@@ -13,6 +13,15 @@ export default defineConfig({
       overwrite: false,
       html: true,
       json: true,
+      charts: true,
+      timestamp: "yyyy-mm-dd_HH-MM-ss",
+      inlineAssets: true,
+      saveJson: true,
+      quiet: true,
+      video: true,
+      videoLink: true,
+      reportTitle: "Cypress Test Report",
+      reportPageTitle: "Cypress Test Results",
     },
     env: {},
   },
